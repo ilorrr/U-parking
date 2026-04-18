5f167925c078a61ab86c373bb7130a527a57bc65
Alex Hinojosa <hinojosa.a.dev@gmail.com>
2026-04-18 15:42:46 -0500

    Delete backend directory

--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import spaces_api, metrics_api, occupancy_update_api
-
-urlpatterns = [
-    path('spaces/', spaces_api),
-    path('metrics/', metrics_api),
-    path("occupancy/update/", occupancy_update_api),
-] 
